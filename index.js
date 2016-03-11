1217349f06c3b0dcdcd7898cbedc788249c9d286
Francis Brito <francis@instacarro.com>
2016-03-11 10:03:21 -0400

    Make factory tests pass.
    * Now it throws if `destroy` and `initialize` or `dependencies` options are missing.
    * Now it throws if `destroy` and `initialize` are not functions.
    * Not it throws if `dependencies` can't be `map`-ped.

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 module.exports = createManageable;
 
-function createManageable() {
+function createManageable(options) {
+  options = options || {};
+
+  const destroy = options.destroy;
+  const initialize = options.initialize;
+  const dependencies = options.dependencies;
+
+  if (destroy && initialize) {
+    if (!(destroy.call && initialize.call)) {
+      throw new Error('`destroy` and `initialize` must be functions.');
+    }
+  } else if (dependencies) {
+    if (!dependencies.map) {
+      throw new Error('`dependencies` must respond to `map` method.');
+    }
+  } else {
+    throw new Error('Either provide `destroy` and `initialize` or `dependencies` options.');
+  }
+
   return {};
 }
